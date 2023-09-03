1203467bf54ffe576390ca71eb883e6d69c6a375
Arius Sulpice <ssodoloufo@gmail.com>
2023-09-03 23:55:05 +0100

    First commit: Initialisation

---
+++ src/main/java/com/ssodoloufo/cms/CustomersManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ssodoloufo.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssodoloufo/cms/CustomersManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssodoloufo.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
